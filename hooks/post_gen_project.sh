14f17d2c75bd2a5571ed403b995a0c9ad0227036
Brian Pugh <bnp117@gmail.com>
2020-01-03 15:32:52 -0800

    gitmodules update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,4 +2,6 @@
 
 git init
 git add .
+git submodule add git@github.com:joltwallet/jolt_wallet.git
+git submodule update --init --recursive
 git commit -a -m "Initial Cookiecutter Commit ( 0.1.2 )."
